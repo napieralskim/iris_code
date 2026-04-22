7483bfa924a7fbc326c66fedc90c32b9981096b4
napieralskim <01189148@pw.edu.pl>
2026-04-23 01:22:17 +0200

    feat: morpho + centroid

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@
 #   swaymsg 'for_window [app_id = "org.matplotlib.*"] floating enable'
 
 import logging
-import matplotlib.figure as mpfig
-import matplotlib.axes as mpaxes
-import matplotlib.image as mpimg
+import matplotlib.figure as figure
+import matplotlib.axes as axes
+import matplotlib.image as image
+import matplotlib.patches as patches
 import matplotlib.pyplot as plot
 import numpy as np
-import skimage
+import skimage as ski
 import sys
 
 
@@ -36,30 +37,49 @@ def img_grayscale(img: np.ndarray) -> np.ndarray:
 
 
 def img_binarize(img_gray: np.ndarray, thresh_scale: float = 1.0) -> np.ndarray:
-    thresh = skimage.filters.threshold_otsu(img_gray)
+    thresh = ski.filters.threshold_otsu(img_gray)
     thresh *= thresh_scale
-    img_binary = img_gray > thresh
-    return img_binary
+    img_bin = img_gray < thresh
+    return img_bin
 
 
-def img_process(img: np.ndarray) -> np.ndarray:
+def img_morpho(mask: np.ndarray, thresh_hole: int, thresh_object: int) -> np.ndarray:
+    mask_new = ski.morphology.remove_small_holes(mask, max_size=thresh_hole)
+    mask_new = ski.morphology.remove_small_objects(mask_new, max_size=thresh_object)
+    return mask_new
+
+
+def img_centroid(mask: np.ndarray) -> tuple[float, float]:
+    centroid = np.argwhere(mask == 1).mean(axis=0)
+    return centroid[1], centroid[0]
+
+
+def img_process(img: np.ndarray) -> tuple[np.ndarray, tuple[float, float]]:
     img = img_grayscale(img)
-    img = img_binarize(img, 0.4) # TODO unhardcode
-    return img
+    img = img_binarize(img, 0.35) # TODO unhardcode
+    img = img_morpho(img, thresh_hole=64, thresh_object=1024) # TODO
+    pupil_center = img_centroid(img)
+    return (img, pupil_center)
 
 
 def img_plot(
     img_path: str, img_title: str,
-    plot_img: mpimg.AxesImage | None, plot_fig: mpfig.Figure, plot_axes: mpaxes.Axes
-) -> mpimg.AxesImage:
-    img = mpimg.imread(img_path)
-    img = img_process(img)
+    plot_img: image.AxesImage | None, plot_fig: figure.Figure, plot_axes: axes.Axes
+) -> image.AxesImage:
+    img_raw = image.imread(img_path)
+    img, pupil_center = img_process(img_raw)
+
     plot_axes.set_title(img_title)
     if plot_img is None:
-        plot_img = plot_axes.imshow(img)
+        plot_img = plot_axes.imshow(img_raw)
     else:
-        plot_img.set_data(img)
+        plot_img.set_data(img_raw)
         plot_axes.figure.canvas.draw_idle()
+
+    for patch in plot_axes.patches:
+        patch.remove()
+    plot_axes.add_patch(
+        patches.Circle(pupil_center, radius=2, color='red', fill=True))
     return plot_img
 
 
@@ -70,7 +90,7 @@ def main():
         sys.exit(1)
 
     plot_fig, plot_axes = plot.subplots()
-    plot_img: mpimg.AxesImage | None = None
+    plot_img: image.AxesImage | None = None
     plot_axes.axis('off')
 
     img_index = 0
@@ -79,7 +99,7 @@ def main():
 
     def key_press_handle(event):
         nonlocal img_index
-        key_bindings = { 'left': -1, 'right': +1, 'h': -1, 's': +1 }
+        key_bindings = { 'left': -1, 'right': +1 }
         if event.key not in key_bindings:
             return
         img_index += key_bindings[event.key]
