90f8a84e0848f9ff514182d38c8b2e4dfe440caf
napieralskim <01189148@pw.edu.pl>
2026-04-23 00:35:20 +0200

    feat: slideshow

--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@
 #   swaymsg 'for_window [app_id = "org.matplotlib.*"] floating enable'
 
 import logging
-from   logging import Logger
+import matplotlib.figure as mpfig
+import matplotlib.axes as mpaxes
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plot
 import numpy as np
+import skimage
 import sys
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -17,8 +20,7 @@ logging.basicConfig(
     format = "%(levelname)s: %(message)s")
 
 
-def grayscale(img: np.ndarray):
-
+def img_grayscale(img: np.ndarray) -> np.ndarray:
     wr = 0.299
     wg = 0.587
     wb = 0.114
@@ -33,20 +35,61 @@ def grayscale(img: np.ndarray):
     return gray
 
 
-def main():
+def img_binarize(img_gray: np.ndarray, thresh_scale: float = 1.0) -> np.ndarray:
+    thresh = skimage.filters.threshold_otsu(img_gray)
+    thresh *= thresh_scale
+    img_binary = img_gray > thresh
+    return img_binary
 
-    if len(sys.argv) < 2:
-        logger.fatal("No image specified")
+
+def img_process(img: np.ndarray) -> np.ndarray:
+    img = img_grayscale(img)
+    img = img_binarize(img, 0.4) # TODO unhardcode
+    return img
+
+
+def img_plot(
+    img_path: str, img_title: str,
+    plot_img: mpimg.AxesImage | None, plot_fig: mpfig.Figure, plot_axes: mpaxes.Axes
+) -> mpimg.AxesImage:
+    img = mpimg.imread(img_path)
+    img = img_process(img)
+    plot_axes.set_title(img_title)
+    if plot_img is None:
+        plot_img = plot_axes.imshow(img)
+    else:
+        plot_img.set_data(img)
+        plot_axes.figure.canvas.draw_idle()
+    return plot_img
+
+
+def main():
+    img_paths = sys.argv[1:]
+    if len(img_paths) < 1:
+        logger.fatal("No image specified!")
         sys.exit(1)
 
-    img_path = sys.argv[1]
-    logger.info(f"Using image: {img_path}")
-    img = mpimg.imread(img_path)
+    plot_fig, plot_axes = plot.subplots()
+    plot_img: mpimg.AxesImage | None = None
+    plot_axes.axis('off')
 
-    img = grayscale(img)
+    img_index = 0
+    img_titler = lambda i: f"[{i + 1}/{len(img_paths)}] {img_paths[i]}"
+    plot_img = img_plot(img_paths[0], img_titler(0), plot_img, plot_fig, plot_axes)
 
-    plot.imshow(img)
-    plot.axis('off')
+    def key_press_handle(event):
+        nonlocal img_index
+        key_bindings = { 'left': -1, 'right': +1, 'h': -1, 's': +1 }
+        if event.key not in key_bindings:
+            return
+        img_index += key_bindings[event.key]
+        img_index %= len(img_paths)
+
+        img_path = img_paths[img_index]
+        img_title = img_titler(img_index)
+        img_plot(img_path, img_title, plot_img, plot_fig, plot_axes)
+
+    plot_fig.canvas.mpl_connect('key_press_event', key_press_handle)
     plot.show()
 
 if __name__ == "__main__":
